8e8b89dba7fb1535e2198c5205f4d27d97786018
RichardG <richard20105@hotmail.com>
2024-06-18 21:55:08 -0500

    Primer Commit

---
+++ src/main/java/com/springboot/prueba/tecnica/SprinbootPruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.prueba.tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootPruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootPruebaTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/prueba/tecnica/SprinbootPruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.prueba.tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootPruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
